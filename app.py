f846ac7af2bfd5a185bab20f4db2ca8f8e08c792
Hjalmar Held <hjalmarheld@gmail.com>
2022-12-06 19:44:04 +0100

    more features

--- app.py
+++ app.py
@@ -1,44 +1,103 @@
 import folium
 import tempfile
+import pandas as pd
 import streamlit as st
 from folium.plugins import Draw
 from streamlit_folium import st_folium
 from picture_fetch import *
 
+# sets download session state to false
 
-# draw map zoomed on france
-m = folium.Map(location=[47.0810, 2.3988], zoom_start=6)
+for state in ['download', 'accepted']:
+    if state not in st.session_state:
+        st.session_state[state] = False
 
-# allow drawing on map
-Draw(
-    draw_options={i:False for i in ['polygon', 'circle', 'rectangle', 'polyline', 'circlemarker']},
-    export=True
-    ).add_to(m)
+#
+# Creates the folium map
+#
 
-# get dropped points on map and return coordinates
-output = st_folium(m, width=800, height=600, returned_objects=["last_active_drawing"])
+col1, col2 = st.columns([7,2])
 
-# set false statefulness until point selected
-if "load_state" not in st.session_state:
-        st.session_state.load_state = False
+with col1:
+    # draw map zoomed on france
+    m = folium.Map(location=[47.0810, 4.3988], zoom_start=5.7)
 
-# keep session state if point selected
-if output['last_active_drawing'] or st.session_state.load_state:
+    # read data on food production in France
+    data = pd.read_csv('data/regions_data.csv')
+    folium.Choropleth(
+        geo_data='data/regions.geojson',
+        data = data,
+        columns= ('Regions', 'Food Production'),
+        key_on='feature.properties.Region',
+        legend_name="Food Production",
+        ).add_to(m)
 
-    # set state to true
-    st.session_state.load_state = True
+    # allow drawing on map
+    Draw(
+        draw_options={i:False for i in ['polygon', 'circle', 'rectangle', 'polyline', 'circlemarker']},
+        export=True
+        ).add_to(m)
 
-    # calculate square around point
-    bbox = draw_square(output['last_active_drawing']['geometry']['coordinates'])
+    # get dropped points on map and return coordinates
+    if not st.session_state.download:
+        output = st_folium(m, width=800, height=600, returned_objects=["last_active_drawing"])
 
-    # download picture
+with col2:
+    st.markdown(
+        '''Place a pin 📍
+        on the map to 
+        analyse an area
+        for silos.
+        ''')
+
+
+#
+# Downloads image
+#
+
+# set true state if point selected and bbox found
+if not st.session_state.download:
+    try:
+        bbox = draw_square(output['last_active_drawing']['geometry']['coordinates'])
+        st.session_state.download = True
+    except:
+        st.session_state.download = False
+
+# start if succesfully found square for image
+if st.session_state.download:
+    # download picture and save to temp dict
     with tempfile.TemporaryDirectory() as temp_dir:
         picture_path = download_picture(
             bbox=bbox,
             dir_name=temp_dir)
 
-        #print(picture_path)
+        # display image
+        col3, col4 = st.columns([7,2])
+        with col3:
+            st.image(picture_path)
 
-        st.image(picture_path)
+        with col4:
+            st.markdown(
+                '''Click yes if you
+                want to analyse 
+                this area.''')
 
-    
+            st.markdown(
+                '''Otherwise, place 
+                a new pin on
+                the map.''')
+            
+            if st.button('Yes'):
+                st.session_state.accepted = True
+
+    # throw away image if user keeps scrolling
+    st.session_state.download = False
+
+
+#
+# Apply deep learning
+#
+
+if st.session_state.accepted:
+    st.markdown('We need a model now')
+    st.video('https://youtu.be/E8gmARGvPlI')
