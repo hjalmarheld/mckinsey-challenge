9d79aecd34cfa114b035ce69322d198953b2b449
Hjalmar Held <hjalmarheld@gmail.com>
2022-12-06 21:42:39 +0100

    picture upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import folium
+import time
 import tempfile
 import pandas as pd
 import streamlit as st
@@ -6,49 +7,75 @@ from folium.plugins import Draw
 from streamlit_folium import st_folium
 from picture_fetch import *
 
-# sets download session state to false
-
-for state in ['download', 'accepted']:
+for state in ["download", "accepted"]:
     if state not in st.session_state:
         st.session_state[state] = False
+
+st.sidebar.markdown('''
+    # Foodix
+
+    Use deep learning to find silos.
+
+    Silos are essential for food storage and famine reduction.
+
+    The interface to right allows you see to search for silos using 
+    satelite pictures. Follow the steps to download a picture from anywhere in France.
+
+    You may also upload an image of your choice below. 
+
+    A size corresponding to 128x128 meters is recomended. 
+''')
+
+if picture_path:=st.sidebar.file_uploader(label=''):
+    st.session_state.download=True
 
 #
 # Creates the folium map
 #
 
-col1, col2 = st.columns([7,2])
+col1, col2 = st.columns([7, 2])
 
 with col1:
     # draw map zoomed on france
     m = folium.Map(location=[47.0810, 4.3988], zoom_start=5.7)
 
     # read data on food production in France
-    data = pd.read_csv('data/regions_data.csv')
+    data = pd.read_csv("data/regions_data.csv")
     folium.Choropleth(
-        geo_data='data/regions.geojson',
-        data = data,
-        columns= ('Regions', 'Food Production'),
-        key_on='feature.properties.Region',
-        legend_name="Food Production",
-        ).add_to(m)
+        geo_data="data/regions.geojson",
+        data=data,
+        columns=("Regions", "Food Production"),
+        key_on="feature.properties.Region",
+        legend_name="Food Index",
+    ).add_to(m)
 
     # allow drawing on map
     Draw(
-        draw_options={i:False for i in ['polygon', 'circle', 'rectangle', 'polyline', 'circlemarker']},
-        export=True
-        ).add_to(m)
+        draw_options={
+            i: False
+            for i in ["polygon", "circle", "rectangle", "polyline", "circlemarker"]
+        },
+        export=True,
+    ).add_to(m)
 
     # get dropped points on map and return coordinates
     if not st.session_state.download:
-        output = st_folium(m, width=800, height=600, returned_objects=["last_active_drawing"])
+        output = st_folium(
+            m, width=800, height=600, returned_objects=["last_active_drawing"]
+        )
+    else:
+        st_folium(
+            m, width=800, height=600, returned_objects=["last_active_drawing"]
+        )
 
 with col2:
     st.markdown(
-        '''Place a pin 📍
+        """Place a pin 📍
         on the map to 
         analyse an area
         for silos.
-        ''')
+        """
+    )
 
 
 #
@@ -58,7 +85,7 @@ with col2:
 # set true state if point selected and bbox found
 if not st.session_state.download:
     try:
-        bbox = draw_square(output['last_active_drawing']['geometry']['coordinates'])
+        bbox = draw_square(output["last_active_drawing"]["geometry"]["coordinates"])
         st.session_state.download = True
     except:
         st.session_state.download = False
@@ -67,27 +94,30 @@ if not st.session_state.download:
 if st.session_state.download:
     # download picture and save to temp dict
     with tempfile.TemporaryDirectory() as temp_dir:
-        picture_path = download_picture(
-            bbox=bbox,
-            dir_name=temp_dir)
+        if not picture_path:
+            picture_path = download_picture(bbox=bbox, dir_name=temp_dir + "/")
+        image_array = load_image(picture_path=picture_path)
 
         # display image
-        col3, col4 = st.columns([7,2])
+        col3, col4 = st.columns([7, 2])
         with col3:
             st.image(picture_path)
 
         with col4:
             st.markdown(
-                '''Click yes if you
+                """Click yes if you
                 want to analyse 
-                this area.''')
+                this area."""
+            )
 
             st.markdown(
-                '''Otherwise, place 
+                """Otherwise, place 
                 a new pin on
-                the map.''')
-            
-            if st.button('Yes'):
+                the map or upload
+                another image."""
+            )
+
+            if st.button("Yes"):
                 st.session_state.accepted = True
 
     # throw away image if user keeps scrolling
@@ -99,5 +129,10 @@ if st.session_state.download:
 #
 
 if st.session_state.accepted:
-    st.markdown('We need a model now')
-    st.video('https://youtu.be/E8gmARGvPlI')
+    print(image_array.shape)
+    with st.spinner('Wait for it...'):
+        time.sleep(5)
+    st.balloons()
+    st.markdown("We need a model now")
+    st.video("https://youtu.be/E8gmARGvPlI")
+    st.session_state.accepted = False
--- picture_fetch.py
+++ picture_fetch.py
@@ -1,4 +1,6 @@
 import urllib.request
+import numpy as np
+from PIL import Image
 
 
 def draw_square(coords: list):
@@ -8,16 +10,34 @@ def draw_square(coords: list):
     long = coords[0]
     lat = coords[1]
 
-    bbox = ','.join([str(round(cord,6)) for cord in [lat-0.001, long-0.001, lat+0.001, long+0.001,]])
+    bbox = ",".join(
+        [
+            str(round(cord, 6))
+            for cord in [
+                lat - 0.001,
+                long - 0.001,
+                lat + 0.001,
+                long + 0.001,
+            ]
+        ]
+    )
 
     return bbox
 
 
 def download_picture(bbox: str, dir_name: str):
-    picture_path = dir_name + '/pic.jpg'
+    picture_path = dir_name + "pic.jpg"
 
-    url = 'https://wxs.ign.fr/ortho/geoportail/r/wms?LAYERS=HR.ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&STYLES=&CRS=EPSG:4326&WIDTH=4000&HEIGHT=4000&BBOX='
+    url = "https://wxs.ign.fr/ortho/geoportail/r/wms?LAYERS=HR.ORTHOIMAGERY.ORTHOPHOTOS&EXCEPTIONS=text/xml&FORMAT=image/jpeg&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&STYLES=&CRS=EPSG:4326&WIDTH=4000&HEIGHT=4000&BBOX="
     url += bbox
     urllib.request.urlretrieve(url, picture_path)
 
     return picture_path
+
+
+def load_image(picture_path):
+    img = Image.open(picture_path)
+    img.load()
+    img = img.resize((256, 256))
+    data = np.asarray(img, dtype="int32")
+    return data
